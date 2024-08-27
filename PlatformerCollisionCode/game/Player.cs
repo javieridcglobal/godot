0124d49ab1a1bac6b230e1c469f0a0255e8f59c7
Javier Santos <javier.idcglobal@gmail.com>
2024-08-27 20:01:17 +0200

    Plataformas con deteccion de colisiones y input. Funcionando.

--- PlatformerCollisionCode/game/Player.cs
+++ PlatformerCollisionCode/game/Player.cs
@@ -1,46 +1,38 @@
 using Godot;
 using System;
 
-public partial class Player : Area2D
+public partial class Player : CharacterBody2D
 {
-
-	    private const int MoveSpeed = 100;
-
-    // All three of these change for each paddle.
-    private int _ballDir;
-    private string _up;
-    private string _down;
+    float gravity = 200f;
+    float TERMINAL_VELOCITY= 1000f;
+    RayCast2D floorDetector = null;
 
     public override void _Ready()
     {
-        string name = Name.ToString().ToLower();
-        _up = "move_up";
-        _down = "move_down";
+        floorDetector = GetNode<RayCast2D>("RayCast2D");
     }
 
-    public override void _Process(double delta)
+    public override void _PhysicsProcess(double delta)
     {
+        float VelocityY = MathF.Min(TERMINAL_VELOCITY, Velocity.Y + gravity * (float)delta);
 
-        float inputY = 0;
-        float inputX = 0;
-        // Move up and down based on input.
-        if (Input.IsKeyPressed(Key.Up)){
-            inputY += -1;
-        }
-        if (Input.IsKeyPressed(Key.Down)){
-            inputY += 1;
-        }
+        float velocityX = 0f;
         if (Input.IsKeyPressed(Key.Right)){
-            inputX += 1;
+            velocityX += 50;
         }
         if (Input.IsKeyPressed(Key.Left)){
-            inputX += -1;
+            velocityX += -50;
         }
-        Vector2 position = Position; // Required so that we can modify position.y.
-        position += new Vector2(inputX * MoveSpeed * (float)delta, inputY * MoveSpeed * (float)delta);
-        position = new(position.X, Mathf.Clamp(position.Y, 16, GetViewportRect().Size.X - 16));
-        Position = position;
-    }
+        if (Input.IsKeyPressed(Key.Up) && floorDetector.IsColliding()){
+            VelocityY += -30;
+        }
 
+        GD.Print("Colliding: " + floorDetector.IsColliding());
+        Velocity = new Vector2(velocityX, VelocityY);
+        //GD.Print("Physicsprocess " + Velocity );
+        MoveAndSlide();
+
+
+    }
 
 }
