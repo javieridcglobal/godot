b30e0b12ab06dd005265983ec2146c0d3a72261e
Javier Santos <javier.idcglobal@gmail.com>
2024-08-28 21:00:13 +0200

    Shooting bullets to the mouse

--- PlatformerCollisionCode/game/Player.cs
+++ PlatformerCollisionCode/game/Player.cs
@@ -4,18 +4,21 @@ using System;
 public partial class Player : Area2D
 {
 
-	    private const int MoveSpeed = 100;
+	private const int MoveSpeed = 100;
+    private const int BULLET_VELOCITY = 400;
 
     // All three of these change for each paddle.
     private int _ballDir;
     private string _up;
     private string _down;
+    PackedScene bulletScene;
 
     public override void _Ready()
     {
         string name = Name.ToString().ToLower();
         _up = "move_up";
         _down = "move_down";
+        bulletScene = GD.Load<PackedScene>("res://bullet.tscn");
     }
 
     public override void _Process(double delta)
@@ -36,11 +39,26 @@ public partial class Player : Area2D
         if (Input.IsKeyPressed(Key.Left)){
             inputX += -1;
         }
+        if (Input.IsActionJustPressed("click")){
+            shoot();
+        }
         Vector2 position = Position; // Required so that we can modify position.y.
         position += new Vector2(inputX * MoveSpeed * (float)delta, inputY * MoveSpeed * (float)delta);
         position = new(position.X, Mathf.Clamp(position.Y, 16, GetViewportRect().Size.X - 16));
         Position = position;
     }
 
+    public void shoot(){
 
+        RigidBody2D bulletInstance = bulletScene.Instantiate<RigidBody2D>();
+        //bulletInstance.Position = this.Position;
+        Vector2 mousePosition = GetViewport().GetMousePosition();
+        Vector2 direction = (mousePosition - this.Position).Normalized();
+
+	    //bulletInstance.global_position = global_position
+        bulletInstance.LinearVelocity = direction * BULLET_VELOCITY;
+
+	    AddChild(bulletInstance);
+    }
 }
+
